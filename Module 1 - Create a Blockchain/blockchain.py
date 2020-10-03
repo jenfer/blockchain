0d97ec7aad25fb4b862f036b1a1635e4d5653ccc
jenfer <jfernandez.ph@gmail.com>
2020-10-04 03:32:42 +0800

    updates

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -1,15 +1,61 @@
-# Module 1 - Create a Blockchain
-
 # To be installed: Flask & Postman API
 
-"""
-Created on Sun Oct  4 00:29:34 2020
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
 
-@author: jen
-"""
 # Part 1 - Building a Blockchain
 
-# Part 2 - Mining
-
-
-
+class Blockchain:
+    
+    def __init__(self):
+       self.chain = []
+       self.create_block(proof = 1, previous_hash = '0') # sha256 output is a string
+       
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1] # -1 refers to the last index in the list
+    
+    # This is the target threshold set by the network for the miners
+    def proof_of_work(self, previous_proof):
+        new_proof = 1;
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+            
+    # This will return the SHA256 hash value of the block/data
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    # Validation of the chain
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                    return False
+            previous_block = block
+            block_index += 1
+        return True
+        
