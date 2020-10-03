e791f727f1656421c21a3cf5e31222c2beed75d1
jenfer <jfernandez.ph@gmail.com>
2020-10-04 01:26:53 +0800

    updates

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -1,9 +1,15 @@
 # Module 1 - Create a Blockchain
+
+# To be installed: Flask & Postman API
+
 """
 Created on Sun Oct  4 00:29:34 2020
 
 @author: jen
 """
+# Part 1 - Building a Blockchain
+
+# Part 2 - Mining
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # blockchain
+# README
+
+The following tools must be installed:
+
+1. Python 3.8
+2. AnaConda / Spyder
+3. Flask
+4. Postman API
+
+
+You can also use PyCharm if you want a better UI.
